2d9ac50749757875942f6dedfe5d0ae4791a5d8c
ibilalchaudhary <bilalchaudhary478@gmail.com>
2024-02-06 16:17:02 +0500

    updates and optimizations

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,14 +11,14 @@ const upload = catchAsync(async (req, res) => {
   if (!req.file) {
     throw new ApiError(httpStatus.NOT_FOUND, 'No file uploaded');
   }
+
   const { file } = req;
 
   let products = [];
   const duplicateAlternateIDs = [];
   const importedProducts = [];
   const failedProducts = [];
-
-  console.log("file",file)
+  const duplicatedDescriptionIds = [];
 
   if (file.originalname.endsWith('.xlsx')) {
     products = await readXlsxFile(`./uploads/${file.filename}`);
@@ -29,89 +29,261 @@ const upload = catchAsync(async (req, res) => {
   }
 
   products.shift();
+
   try {
     for (const row of products) {
-      const productData = {
-        preferredSupplier: row[0],
-        itemStatus: row[1],
-        inventoryID: row[2],
-        alternateID: row[3],
-        itemClass: row[4],
-        description: row[5],
-        masterCTNQty: row[6],
-        masterBarcode: row[7],
-        masterUOMLength: row[8],
-        masterUOMHeight: row[9],
-        masterUOMWidth: row[10],
-        masterVolumeCubicM: row[11],
-        masterWeightKG: row[12],
-        baseQTY: row[13],
-        baseUOMBarcode: row[14],
-        baseUOMLength: row[15],
-        baseUOMHeight: row[16],
-        baseUOMWidth: row[17],
-        baseVolumeCubicM: row[18],
-        baseWeightKG: row[19],
-        baseUnit: row[20],
-        salesUnit: row[21],
-        purchaseUnit: row[22],
-        lastCost: row[23],
-        defaultPrice: row[24],
-        lotSize: row[25],
-        serialClass: row[26],
-        priceClass: row[27],
-        taxCategory: row[28],
-        brand: row[29],
-        substitute: row[30],
-        crossSell: row[31],
-        upSell: row[32],
-        movementClass: row[33],
-        reorderPoint: row[34],
-        maximumSOH: row[35],
-        cost: row[36],
-        basePrice: row[37],
-        priceLevel1: row[38],
-        priceLevel2: row[39],
-        priceLevel3: row[40],
-        priceLevel4: row[41],
-        priceLevel5: row[42],
-        priceLevel6: row[43],
-      };
-
+      const productData = extractProductData(row);
       const existingProduct = await Product.findOne({ alternateID: productData.alternateID });
 
       if (existingProduct) {
         duplicateAlternateIDs.push(productData.alternateID);
         failedProducts.push(productData);
       } else {
-        const product = new Product(productData);
-        await product.save();
-        importedProducts.push(product);
+        const isDuplicateDescription = await checkDuplicateDescription(productData);
+
+        if (isDuplicateDescription) {
+          duplicatedDescriptionIds.push(productData.alternateID);
+          failedProducts.push(productData);
+        } else {
+          const product = new Product(productData);
+          await product.save();
+          importedProducts.push(product);
+        }
       }
     }
 
-    if (duplicateAlternateIDs.length > 0) {
-      res.status(httpStatus.CONFLICT).send({
-        message: 'Some products could not be imported due to duplicate alternate IDs',
-        importedProducts: importedProducts,
-        duplicateAlternateIDs: duplicateAlternateIDs,
-        failedProducts: failedProducts
+    if (duplicateAlternateIDs.length > 0 || duplicatedDescriptionIds.length > 0) {
+      const errorMessage = duplicateAlternateIDs.length > 0 ?
+        'Some products could not be imported due to duplicate alternate IDs' :
+        'Some products could not be imported due to duplicate description rule';
+
+      res.status(httpStatus.CREATED).send({
+        status: false,
+        message: errorMessage,
+        importedProducts,
+        duplicateAlternateIDs,
+        duplicatedDescriptionIds,
+        failedProducts,
       });
     } else {
       res.status(httpStatus.CREATED).send({
+        status: true,
         message: 'All products imported successfully',
-        importedProducts: importedProducts,
+        importedProducts,
       });
     }
   } catch (error) {
     res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
       message: 'Error importing products',
       error: error.message,
-      importedProducts: importedProducts,
-      failedProducts: failedProducts
+      importedProducts,
+      failedProducts,
     });
   }
 });
+
+const extractProductData = (row) => {
+  return {
+    preferredSupplier: row[0],
+    itemStatus: row[1],
+    inventoryID: row[2],
+    alternateID: row[3],
+    itemClass: row[4],
+    description: row[5],
+    masterCTNQty: row[6],
+    masterBarcode: row[7],
+    masterUOMLength: row[8],
+    masterUOMHeight: row[9],
+    masterUOMWidth: row[10],
+    masterVolumeCubicM: row[11],
+    masterWeightKG: row[12],
+    baseQTY: row[13],
+    baseUOMBarcode: row[14],
+    baseUOMLength: row[15],
+    baseUOMHeight: row[16],
+    baseUOMWidth: row[17],
+    baseVolumeCubicM: row[18],
+    baseWeightKG: row[19],
+    baseUnit: row[20],
+    salesUnit: row[21],
+    purchaseUnit: row[22],
+    lastCost: row[23],
+    defaultPrice: row[24],
+    lotSize: row[25],
+    serialClass: row[26],
+    priceClass: row[27],
+    taxCategory: row[28],
+    brand: row[29],
+    substitute: row[30],
+    crossSell: row[31],
+    upSell: row[32],
+    movementClass: row[33],
+    reorderPoint: row[34],
+    maximumSOH: row[35],
+    cost: row[36],
+    basePrice: row[37],
+    priceLevel1: row[38],
+    priceLevel2: row[39],
+    priceLevel3: row[40],
+    priceLevel4: row[41],
+    priceLevel5: row[42],
+    priceLevel6: row[43],
+  };
+};
+
+const checkDuplicateDescription = async (productData) => {
+  const combinedDescription = productData.brand + productData.description + (productData.lotSize || productData.masterWeightKG);
+  console.log("combinedDescription",combinedDescription)
+  const existingProducts = await Product.find({});
+  return existingProducts.some(existingProduct => {
+    const existingCombinedDescription = existingProduct.brand + existingProduct.description + (existingProduct.lotSize || existingProduct.masterWeightKG);
+    console.log("existingCombinedDescription",existingCombinedDescription)
+    return existingCombinedDescription === combinedDescription;
+  });
+};
+
+// const upload = catchAsync(async (req, res) => {
+//   if (!req.file) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'No file uploaded');
+//   }
+//   const { file } = req;
+//
+//   let products = [];
+//   const duplicateAlternateIDs = [];
+//   const importedProducts = [];
+//   const failedProducts = [];
+//   let dublicatedDescriptionId = []
+//
+//   console.log("file",file)
+//
+//   if (file.originalname.endsWith('.xlsx')) {
+//     products = await readXlsxFile(`./uploads/${file.filename}`);
+//   } else if (file.originalname.endsWith('.csv')) {
+//     products = await csv().fromFile(`./uploads/${file.filename}`);
+//   } else {
+//     throw new ApiError(httpStatus.UNSUPPORTED_MEDIA_TYPE, 'Unsupported file format');
+//   }
+//
+//   products.shift();
+//   try {
+//     for (const row of products) {
+//       const productData = {
+//         preferredSupplier: row[0],
+//         itemStatus: row[1],
+//         inventoryID: row[2],
+//         alternateID: row[3],
+//         itemClass: row[4],
+//         description: row[5],
+//         masterCTNQty: row[6],
+//         masterBarcode: row[7],
+//         masterUOMLength: row[8],
+//         masterUOMHeight: row[9],
+//         masterUOMWidth: row[10],
+//         masterVolumeCubicM: row[11],
+//         masterWeightKG: row[12],
+//         baseQTY: row[13],
+//         baseUOMBarcode: row[14],
+//         baseUOMLength: row[15],
+//         baseUOMHeight: row[16],
+//         baseUOMWidth: row[17],
+//         baseVolumeCubicM: row[18],
+//         baseWeightKG: row[19],
+//         baseUnit: row[20],
+//         salesUnit: row[21],
+//         purchaseUnit: row[22],
+//         lastCost: row[23],
+//         defaultPrice: row[24],
+//         lotSize: row[25],
+//         serialClass: row[26],
+//         priceClass: row[27],
+//         taxCategory: row[28],
+//         brand: row[29],
+//         substitute: row[30],
+//         crossSell: row[31],
+//         upSell: row[32],
+//         movementClass: row[33],
+//         reorderPoint: row[34],
+//         maximumSOH: row[35],
+//         cost: row[36],
+//         basePrice: row[37],
+//         priceLevel1: row[38],
+//         priceLevel2: row[39],
+//         priceLevel3: row[40],
+//         priceLevel4: row[41],
+//         priceLevel5: row[42],
+//         priceLevel6: row[43],
+//       };
+//
+//       const existingProduct = await Product.findOne({ alternateID: productData.alternateID });
+//
+//       if (existingProduct) {
+//         duplicateAlternateIDs.push(productData.alternateID);
+//         failedProducts.push(productData);
+//       } else {
+//         const product = new Product(productData);
+//         await product.save();
+//         importedProducts.push(product);
+//       }
+//
+//
+//
+//       const combinedDescription = productData.brand + productData.description + (productData.lotSize || productData.masterWeightKG);
+//       const existingProducts = await Product.find({});
+//
+//       let isDuplicate = false;
+//       for (let existingProduct of existingProducts) {
+//         const existingCombinedDescription = existingProduct.brand + existingProduct.description + (existingProduct.lotSize || existingProduct.masterWeightKG);
+//         if (existingCombinedDescription === combinedDescription) {
+//           isDuplicate = true;
+//           break;
+//         }
+//       }
+//
+//       if (isDuplicate) {
+//         dublicatedDescriptionId.push(productData.alternateID);
+//         failedProducts.push(productData);
+//       } else {
+//         const product = new Product(productData);
+//         await product.save();
+//         importedProducts.push(product);
+//       }
+//
+//     }
+//
+//     if (duplicateAlternateIDs.length > 0) {
+//       res.status(httpStatus.CREATED).send({
+//         status:false,
+//         message: 'Some products could not be imported due to duplicate alternate IDs',
+//         importedProducts: importedProducts,
+//         duplicateAlternateIDs: duplicateAlternateIDs,
+//         failedProducts: failedProducts
+//       });
+//
+//       if (dublicatedDescriptionId.length > 0) {
+//         res.status(httpStatus.CREATED).send({
+//           status: false,
+//           message: 'Some products could not be imported due to duplicate description rule',
+//           importedProducts: importedProducts,
+//           dublicatedDescriptionId: dublicatedDescriptionId,
+//           failedProducts: failedProducts
+//         });
+//       }
+//     } else {
+//       res.status(httpStatus.CREATED).send({
+//         status:true,
+//         message: 'All products imported successfully',
+//         importedProducts: importedProducts,
+//       });
+//     }
+//   } catch (error) {
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+//       message: 'Error importing products',
+//       error: error.message,
+//       importedProducts: importedProducts,
+//       failedProducts: failedProducts
+//     });
+//   }
+// });
 
 const addProduct = catchAsync(async (req, res) => {
   try {
